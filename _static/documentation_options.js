99c8c5e6d75a5aca91d2c1d2027d5a1a3d40d915
psilo909 <psilo909@users.noreply.github.com>
2022-05-16 07:49:46 +0000

    Deploying to gh-pages from @ smarthomeNG/smarthome@dc698c346400f4c2655e43ec887f8e9cf80fac52 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.9.2.1 develop (Stand 16. May 2022, commit d017e84)',
+    VERSION: 'v1.9.2.1 develop (Stand 16. May 2022, commit dc698c3)',
     LANGUAGE: 'de',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
