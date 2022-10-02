29cc91d99a0020f81358fca8ff84155feaca602f
onkelandy <onkelandy@hotmail.com>
2022-10-02 21:57:11 +0000

    Deploying to gh-pages from @ smarthomeNG/plugins@c7c5de6669e6bcc9d77434c446db556180b4d03e 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.9.2.2 develop (Stand 01. October 2022, commit 78229f6)',
+    VERSION: 'v1.9.2.2 develop (Stand 02. October 2022, commit 78229f6)',
     LANGUAGE: 'de',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
