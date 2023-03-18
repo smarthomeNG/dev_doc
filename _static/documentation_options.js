f5d921aa1cc24c3b170a80bad181abab5ccded13
bmxp <Bernd.Meiners@mail.de>
2023-03-18 09:44:40 +0000

    Deploying to gh-pages from @ smarthomeNG/smarthome@9305c82b03b1dacc62ea565e242acbcac37b5f34 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.9.4.1 develop (Stand 17. March 2023, commit 97a1144)',
+    VERSION: 'v1.9.4.1 develop (Stand 18. March 2023, commit 9305c82)',
     LANGUAGE: 'de',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
