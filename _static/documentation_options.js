cd918023439f9ae9afcb60f31f5eb8453802c3f8
msinn <msinn@users.noreply.github.com>
2023-08-03 10:37:21 +0000

    Deploying to gh-pages from @ smarthomeNG/smarthome@fca4ededf452a6e19888c7a34b0c2104b2e53c1a 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.9.5.4 develop (Stand 31. März 2023, commit f937924)',
+    VERSION: 'v1.9.5.4 develop (Stand 31. März 2023, commit fca4ede)',
     LANGUAGE: 'de',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
