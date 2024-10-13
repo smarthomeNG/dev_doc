837c17e5e4d3c1e8fefbf7f5ac64962762038738
Morg42 <43153739+Morg42@users.noreply.github.com>
2024-10-13 15:19:03 +0000

    Deploying to gh-pages from @ smarthomeNG/smarthome@eb99adb16b9699447a8c53f9f7fdf87398ab2473 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.10.0.3 develop (Stand 11. Oktober 2024, commit 0790565)',
+    VERSION: 'v1.10.0.3 develop (Stand 13. Oktober 2024, commit eb99adb)',
     LANGUAGE: 'de',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
