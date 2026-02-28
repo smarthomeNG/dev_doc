09148be7d602b581e8cbd603096e1fc2d54b762c
aschwith <alex.schwithal@gmx.de>
2026-02-28 13:26:49 +0000

    Deploying to gh-pages from @ smarthomeNG/plugins@0acf14aeda03e8dfc159ab667bc1de1e94df6000 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.11.0.2 develop (Stand 27. Februar 2026, commit 9bd379a)',
+    VERSION: 'v1.11.0.2 develop (Stand 28. Februar 2026, commit 9bd379a)',
     LANGUAGE: 'de',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
